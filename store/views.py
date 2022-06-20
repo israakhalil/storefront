c34b5c193a7aa467fa436033141224bc7d818eca
Israa Khalil <isra2.it.90@gmail.com>
2022-06-20 20:42:41 +0300

    edits

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
+from turtle import title
 from django.shortcuts import render
+from store.models import Collection, Product
 
 # Create your views here.
+
+def say_hello(request):
+    # query_set =Product.objects.filter(unit_price__range=(20,30))
+    query_set =Product.objects.values('id','title','collection__title')
+    return render(request,'hello.html' ,{'name':'Israa', 'products':list(query_set)})
