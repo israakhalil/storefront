61a7e4e22603ed382acf6b377c17f70a301189b0
Israa Khalil <isra2.it.90@gmail.com>
2022-06-21 01:39:40 +0300

    admin

--- store/models.py
+++ store/models.py
@@ -12,6 +12,12 @@ class Collection(models.Model):
     title=models.CharField(max_length=255)
     featured_product=models.ForeignKey('Product', on_delete=models.SET_NULL,null=True,related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering=['title']
+
 class Product(models.Model):
     title = models.CharField(max_length=220)
     slug=models.SlugField()
@@ -21,6 +27,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection=models.ForeignKey(Collection,on_delete=models.PROTECT)
     promotion=models.ManyToManyField(Promotion)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering=['title']
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -39,6 +51,7 @@ class Customer(models.Model):
     birth_date = models.DateField(null=False)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
 
 
 class Order(models.Model):
